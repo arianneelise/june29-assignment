b1c19043f536465ade2c97079a8ff72427eee609
arianneelise <arianne@devpipeline.com>
2022-06-29 13:41:30 -0600

    completed assignment requirements

---
+++ index.js
@@ -0,0 +1,24 @@
+async function swapiPromise(id) {
+  try {
+    const data = await fetch(`https://www.swapi.tech/api/planets/${id}`).then(
+      (res) => res.json()
+    );
+    return data;
+  } catch (err) {
+    return err;
+  }
+}
+
+function displayPlanet() {
+  const input = document.getElementById("planet-input");
+  swapiPromise(input.value)
+    .then((res) => {
+      const planet = res["result"]["properties"]["name"];
+      document.getElementById("planet-name").innerHTML = planet;
+    })
+    .catch(
+      (err) =>
+        (document.getElementById("planet-name").innerHTML =
+          "Fail! Invalid input.")
+    );
+}
